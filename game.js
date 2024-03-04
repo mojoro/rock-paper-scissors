77554eb0b4334e6fadab4d45fa710953489d34f8
mojoro <45500240+mojoro@users.noreply.github.com>
2024-03-05 00:21:36 +0100

    Create core game logic and test

---
+++ game.js
@@ -0,0 +1,57 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random()*10) % 3;
+    if (choice === 0){
+        return "Rock";
+    }
+    if (choice === 1){
+        return "Paper";
+    }
+    if (choice === 2){
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice())
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === "rock"){
+        if (computerSelection === "rock"){
+            return "You tied! Try again.";
+        }
+        else if (computerSelection === "paper"){
+            return "You lose! Paper beats Rock.";
+        }
+        else if (computerSelection === "scissors"){
+            return "You win! Rock beats Scissors.";
+        }
+    }
+    else if (playerSelection === "paper"){
+        if (computerSelection === "rock"){
+            return "You win! Paper beats Rock.";
+        }
+        else if (computerSelection === "paper"){
+            return "You tied! Try again.";
+        }
+        else if (computerSelection === "scissors"){
+            return "You lose! Scissors beats Paper.";
+        }
+    }
+    else if (playerSelection === "scissors"){
+        if (computerSelection === "rock"){
+            return "You lose! Rock beats Scissors.";
+        }
+        else if (computerSelection === "paper"){
+            return "You win! Scissors beats Paper.";
+        }
+        else if (computerSelection === "scissors"){
+            return "You tied! Try again.";
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
